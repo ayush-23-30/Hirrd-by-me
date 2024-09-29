fc42e42534569726a1032928f8eb46523fb0fa68
ayush-23-30 <pawar85060@gmail.com>
2024-09-29 21:44:40 +0530

    n

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
-
 import AppLayout from "./layouts/app-layout";
 import ProtectedRoute from "./components/protected-route";
 import { ThemeProvider } from "./components/theme-provider";
